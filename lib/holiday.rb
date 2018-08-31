8c6d6bc5849eccc5d75debf896b4e2f6a0c4c362
Adam Clayman <adam@clayman.org>
2018-08-31 19:48:14 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,7 +55,12 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, values|
+    puts "#{season}:/n"
+    values.each do |key, attributes|
+      puts "  #{key}: #{attributes}"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
