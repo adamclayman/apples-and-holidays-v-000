7118349084a14f2691e9ffa794b2f4af00b964bc
Adam Clayman <adam@clayman.org>
2018-08-31 20:05:41 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, values|
     puts "#{season.capitalize}:"
     values.each do |key, attributes|
-      puts "  #{key.capitalize}: #{attributes.to_s.strip}"
+      puts "  #{key.capitalize}: #{attributes.join(",")}"
     end
   end
 end
