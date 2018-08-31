33c340ea80093155c1577df479f1d57b65f86d46
Adam Clayman <adam@clayman.org>
2018-08-31 19:52:25 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, values|
     puts "#{season.capitalize}:"
     values.each do |key, attributes|
-      puts "\t#{key.capitalize}: #{attributes}"
+      puts "  #{key.capitalize}: #{attributes}"
     end
   end
 end
